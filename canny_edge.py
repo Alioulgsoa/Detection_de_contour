27dd4fe11cd5a19e3f877e9ec699bd482286146a
aboulgsoa <ali.boulgsoa.96@gmail.com>
2024-01-11 14:03:21 +0100

    Contour detection

--- canny_edge.py
+++ canny_edge.py
@@ -7,9 +7,9 @@ from matplotlib import pyplot as plt
 
 the_image = cv.imread('euro.jpg')
 #resized_img = cv.resize(the_image, (100, 100))
+rgb_img = cv.cvtColor(the_image, cv.COLOR_BGR2RGB)  # convert BGR to RGB
 
-canny_image = cv.Canny(the_image, 100, 200)
-
+canny_image = cv.Canny(rgb_img, 100, 200)
 
 # show image with OpenCV
 '''cv.imshow('canny image', canny_image)
@@ -18,7 +18,7 @@ cv.destroyWindow()'''
 
 #show image with matplotlib
 
-plt.subplot(121), plt.imshow(the_image), plt.title('the Original Image')
+plt.subplot(121), plt.imshow(rgb_img), plt.title('the Original Image')
 plt.xticks([]), plt.yticks([])
 
 plt.subplot(122), plt.imshow(canny_image), plt.title('Detected Image')
---
+++ contour_detection.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+
+theimage = cv.imread('euro.jpg', 0) #le zero pour importer l'image au niveux de gray
+theimage = cv.resize(theimage, (600, 600))
+
+ret, thres = cv.threshold(theimage, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU) # OTSU Threshold
+
+contours, hierarchy = cv.findContours(thres, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) #detection contoure
+
+color_image = cv.cvtColor(theimage, cv.COLOR_GRAY2BGR) # parceque je veux traiter en cv2
+
+theimage = cv.drawContours(color_image, contours, -1, (200, 0, 0), 3) #-1 pour pas remplire , 1 pour l'epesseur
+print("the number of contours :" + str(len(contours)))
+
+# put the number of contours in the image
+cv.putText(theimage, "the number of contours :" + str(len(contours)), (20, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 200), 1)
+
+cv.imshow('theimage', theimage)
+cv.waitKey()
+cv.destroyAllWindows()
+
+
+
